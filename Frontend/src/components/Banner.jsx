027c796040a24de8cef30b9b2de9d09cf6558ba0
Dvarun <varundhurai19@gmail.com>
2024-06-17 23:48:29 +0530

    initial command

---
+++ Frontend/src/components/Banner.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <>
+    <div className="max-w-screen-2xl container mx-auto md:px-20 px-4 flex">
+      <div>Left</div>
+      <div>Right</div>
+    </div>
+    </>
+  )
+}
+
+export default Banner
